659e34a04b2a896b1a6c43782534de83277813f8
Abel Castro <abel.castro9111@gmail.com>
2017-11-21 18:41:35 +0000

    Se carga modal para asignación de accesos

---
+++ app/Http/Controllers/Admin/AccesosController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Acceso;
+use App\User;
+use App\User_Acceso;
+
+class AccesosController extends Controller
+{
+    public function showListForm(User $user) {
+        $accesos = Acceso::All();
+        $userAccesos = $user->accesos->pluck('id')->toArray();
+        return view('admin.acceso._listForm', compact('accesos', 'userAccesos', 'user'));
+    }
+
+    public function putListForm(User $user, Request $request) {
+        $accesos = $request->input('accesos');
+        $userAcceso = [];
+        foreach ($accesos as $acceso) {
+            $userAcceso [$acceso] = [
+                'created_by' => '1',
+                'updated_by' => '1'
+            ];
+        }
+        $user->accesos()->sync($userAcceso);
+        return 'Guardado con exito';
+    }
+}
