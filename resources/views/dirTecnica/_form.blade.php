4f8337f64e696abfc6144ab90a4347c23770a4c8
Abel Castro <abel.castro9111@gmail.com>
2017-11-27 22:17:07 +0000

    Se aplica correcciones a la vista director tecnico

--- resources/views/dirTecnica/_form.blade.php
+++ resources/views/dirTecnica/_form.blade.php
@@ -1,73 +1,69 @@
+<div class="col-md-12 row">
+  <div class="form-group col-md-6">
+       <label class="">Turno</label>
+       {{Form::select(
+           'turnos[]',
+           $turnos,
+           null,
+           ['class' => 'form-control turnosSelect2', 'multiple' => 'multiple'])}}
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Número de expediente</label>
+       <input type="text" class="form-control" name="numExp[{{$turno or ''}}]">
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Nombre del (la) Director(a) Técnico(a)</label>
+       <input type="text" class="form-control" name="nombreDirector[{{$turno or ''}}]">
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Grado académico y nombre de estudio</label>
+       <input type="text" class="form-control" name="grado[{{$turno or ''}}]">
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Horario de permanencia en la ISI (rango)</label>
+       <input type="text" class="form-control" name="horario[{{$turno or ''}}]">
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Antigüedad docente DGIRE</label>
+       <input type="text" class="form-control" name="antDocente[{{$turno or ''}}]">
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Fecha de oficio de autorizacion como Director Técnico</label>
+       <input type="text" class="form-control" name="FechaOficio[{{$turno or ''}}]">
+  </div>
+  <div class="form-group col-md-6">
+       <label for="">Antigüedad en la Dirección Tecnica de la ISI (años)</label>
+       <input type="text" class="form-control" name="FechaOficio[{{$turno or ''}}]">
+  </div>
+</div>
 <div class="col-md-12">
-     <div class="col-md-10 col-md-offset-1">
-          <div class="form-group col-md-6">
-               <label class="">Turno</label>
-               <select class="form-control">
-                    <option>Matutino</option>
-                    <option>Vespertino</option>
-                    <option>Mixto</option>
-               </select>
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Nombre del (la) Director(a) Técnico(a)</label>
-               <input type="text" class="form-control" name="nombreDirector[{{$turno or ''}}]">
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Grado academico y nombre de estudio</label>
-               <input type="text" class="form-control" name="grado[{{$turno or ''}}]">
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Horario de permanencia en la ISI (rango)</label>
-               <input type="text" class="form-control" name="horario[{{$turno or ''}}]">
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Antigüedad docente DGIRE</label>
-               <input type="text" class="form-control" name="antDocente[{{$turno or ''}}]">
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Número de expediente</label>
-               <input type="text" class="form-control" name="numExp[{{$turno or ''}}]">
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Fecha de oficio de autorizacion como Director Técnico</label>
-               <input type="text" class="form-control" name="FechaOficio[{{$turno or ''}}]">
-          </div>
-          <div class="form-group col-md-6">
-               <label for="">Antigüedad en la Dirección Tecnica de la ISI (años)</label>
-               <input type="text" class="form-control" name="FechaOficio[{{$turno or ''}}]">
-          </div>
-
-          <table class="table" id="horarios">
-               <thead>
-                    <tr>
-                         <th colspan="3">Asignaturas y horarios que se imparte en el plan UNAM</th>
-                    </tr>
-                    <tr>
-                         <th>Clave Asignatura</th>
-                         <th>Horario</th>
-                    </tr>
-               </thead>
-               <tbody>
-                    <tr>
-                         <td>
-                              <select class="form-control" name="cveMateria[1]">
-                                   <option value="1511">Ciencias de la salud</option>
-                                   <option value="1512">Ciencias politicas y sociales</option>
-                                   <option value="1513">Derecho I</option>
-                                   <option value="1514">Economia I</option>
-                                   <option value="1515">Geografia I</option>
-                                   <option value="1516">Psicologia I</option>
-                              </select>
-                         </td>
-                         <td><input type="text" class="form-control" name="horario[1]"></td>
-                         <td align="right">
-                              <button class="btn btn-danger" type="button" id="eliminar"><i class="glyphicon glyphicon-trash"></i></button>
-                              <button class="btn btn-success" type="button" id="agregar"><i class="glyphicon glyphicon-plus"></i></button>
-                         </td>
-                    </tr>
-               </tbody>
-          </table>
-          <div class="form-group">
-          </div>
-     </div>
+  <table class="table" id="horarios">
+       <thead>
+            <tr>
+                 <th>Clave Asignatura</th>
+                 <th>Horario</th>
+            </tr>
+       </thead>
+       <tbody>
+            <tr>
+                 <td class="col-md-6">
+                      <select class="form-control materiasSelect2" name="cveMateria[1]">
+                           <option value="1511">Ciencias de la salud</option>
+                           <option value="1512">Ciencias politicas y sociales</option>
+                           <option value="1513">Derecho I</option>
+                           <option value="1514">Economia I</option>
+                           <option value="1515">Geografia I</option>
+                           <option value="1516">Psicologia I</option>
+                      </select>
+                 </td>
+                 <td><input type="text" class="form-control" name="horario[1]"></td>
+                 <td align="right">
+                      <button class="btn btn-danger" type="button" id="eliminar"><i class="glyphicon glyphicon-trash"></i></button>
+                      <button class="btn btn-success" type="button" id="agregar"><i class="glyphicon glyphicon-plus"></i></button>
+                 </td>
+            </tr>
+       </tbody>
+  </table>
+  <div class="form-group">
+  </div>
 </div>
