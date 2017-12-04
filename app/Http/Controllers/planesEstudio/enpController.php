cc4469a6f8e09ac5f81c73059f7db6a789baf061
Abel Castro <abel.castro9111@gmail.com>
2017-12-04 17:07:23 +0000

    Se dejan solo asignaturas optativas

--- app/Http/Controllers/planesEstudio/enpController.php
+++ app/Http/Controllers/planesEstudio/enpController.php
@@ -9,56 +9,40 @@ class enpController extends Controller
 {
      public function index() {
           $enp = [
-                "Cuarto" => [
-                   'Obligatorias de Elección – Lengua Extranjera' => [
-                         'Inglés IV',
-                         'Fránces IV'
-                   ],
-                   'Educación Estética Y Artística' => [
-                         'Danza Clásica',
-                         'Danza Contemporánea',
-                         'Danza Española',
-                         'Danza Regional',
-                         'Música',
-                         'Teatro',
-                         'Pintura',
-                         'Escultura',
-                         'Grabado',
-                         'Fotografía'
-                   ]
-                ],
-                "Quinto" => [
-                   'Obligatorias de Elección – Lengua Extranjera' => [
-                         'Inglés V',
-                         'Fránces V',
-                         'Italiano I',
-                         'Alemán I',
-                         'Inlgés I',
-                         'Francés I'
-                   ],
-                   'Educación Estética Y Artística' => [
-                         'Danza Clásica',
-                         'Danza Contemporánea',
-                         'Danza Española',
-                         'Danza Regional',
-                         'Música',
-                         'Teatro',
-                         'Pintura',
-                         'Escultura',
-                         'Grabado',
-                         'Fotografía'
-                   ]
-
-
-               ],
                "Sexto" => [
-                   'Obligatorias de Elección – Lengua Extranjera' => [
-                        'Inglés VI',
-                        'Fránces VI',
-                        'Italiano II',
-                        'Alemán II',
-                        'Inlgés II',
-                        'Francés II'
+                   'Área I: Físico - Matemático' => [
+                       'Geología y Minerología',
+                       'Físico - Química',
+                       'Temas Selectos de Matemáticas',
+                       'Estadística y Probabilidad',
+                       'Informática Aplicada a la Ciencia y a la Industria',
+                       'Cosmografía',
+                       'Biología V'
+                   ],
+                   'Área II: Ciencias Biológicas y de la Salud' => [
+                       'Geología y Minerología',
+                       'Físico - Química',
+                       'Temas Selectos de Biología',
+                       'Estadística y Probabilidad',
+                       'Temas Selectos de Morfofisiología',
+                       'Informática Aplicada a la Ciencia y a la Industria',
+                   ],
+                   'Área III: Ciencias Sociales' => [
+                       'Contabilidad y Gestión Administrativa',
+                       'Geografía Política',
+                       'Estadística y Probabilidad',
+                       'Sociología'
+                   ],
+                   'Área IV: Humanidades y Artes' => [
+                       'Revolución Mexicana',
+                       'Pensamiento Filosófico en México',
+                       'Modelado II',
+                       'Estadística y Probabilidad',
+                       'Latín',
+                       'Griego',
+                       'Comunicación Visual',
+                       'Estética',
+                       'Historia del Arte'
                    ]
                ]
           ];
