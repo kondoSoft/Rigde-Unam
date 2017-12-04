ac3093130787b42ae17a3404a6888153b8cbf59d
Abel Castro <abel.castro9111@gmail.com>
2017-12-04 16:18:39 +0000

    Migration y models de las tablas, material, materialrequerimiento, material_isi, tipoMaterial, unidadMedida

---
+++ database/migrations/2017_12_04_144916_create_materiales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaterialesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materiales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_materialRequerimiento');
+            $table->string('nombre');
+            $table->integer('id_unidadMedida');
+            $table->integer('id_tipoMaterial');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materiales');
+    }
+}
