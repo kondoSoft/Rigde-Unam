ad243d2b2d575fa9309a9ba214f0cd8b0de3d2e9
Abel Castro <abel.castro9111@gmail.com>
2017-12-08 00:47:50 +0000

    Se crea la vista para visualizar indicadores

---
+++ resources/views/catalogo/indicador/_detallesModal.blade.php
@@ -0,0 +1,37 @@
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th colspan="2">Detalles del indicador</th>
+        </tr>
+    </thead>
+    <tr>
+        <th align="right">Nombre</th>
+        <td>Documentos relativos a la seguridad del inmueble, requeridos por las normas federales y municipales.</td>
+    </tr>
+    <tr>
+        <th>Pertinencia</th>
+        <td>La existencia y vigencia de estos documentos, garantizan la habitabilidad, accesibilidad, seguridad y operación del inmueble acorde a la Normatividad vigente, ya que sin estos la ISI no puede operar. </td>
+    </tr>
+    <tr>
+        <th>Objetivo</th>
+        <td>Verificar que la ISI cuente con los documentos relativos a la seguridad del inmueble, requeridos por las normas federales y municipales.</td>
+    </tr>
+    <tr>
+        <th>Factores a considerar</th>
+        <td>
+            Los documentos son expedidos por peritos certificados por las autoridades correspondientes y con registros vigentes a la fecha de elaboración de los respectivos dictámenes.
+El Uso del Suelo (USE) una vez ejercido, deberá actualizarse si exceden los metros cuadrados de construcción autorizados.
+El Dictamen Estructural tiene una vigencia de 5 años.
+El Visto Bueno de Seguridad y Operación tiene una vigencia de 3 años.
+El Dictamen de Instalación Eléctrica tiene una vigencia de 5 años o antes si se modifica la instalación.
+El Dictamen de Instalación de Gas tiene una vigencia de 5 años o antes si se modifica la instalación.
+El Visto Bueno del Programa Interno de Protección Civil tiene una vigencia de 1 año. El Director técnico deberá contar con una bitácora de simulacros programados durante el ciclo escolar y deberá estar actualizada.
+
+Nota 1: Es importante destacar que regirá la Normatividad federal correspondiente, en el caso de que el perito en la materia no justifique técnicamente la vigencia de los dictámenes  o  bien, cuando la reglamentación local tampoco lo señale.
+
+Nota 2: En caso de modificar la estructura y/o las instalaciones, los dictámenes deberán renovarse.
+
+Nota 3: En el caso de ocurrir un sismo intenso deberán renovarse el Dictamen Estructural  y el Visto Bueno de Seguridad y Operación.
+        </td>
+    </tr>
+</table>
---
+++ resources/views/catalogo/indicador/_evaluarModal.blade.php
@@ -0,0 +1,1 @@
+{{ Form::label('formula', 'Fórmula') }}
